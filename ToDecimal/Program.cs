2a4dd5511b9faa092136298d52f2a140b007aaf7
Akn <sibata486@gmail.com>
2021-02-28 21:32:00 +0900

    init

---
+++ ToDecimal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDecimal {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("hex -> decimal : ");
+
+            for(int i = 0; i < args.Length; i++) {
+                Console.Write(Convert.ToInt32(args[i], 16));
+                Console.Write(" ");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
